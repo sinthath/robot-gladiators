13c6c32daa6283d5688a63878415c4632e982f36
sinthath <sinthathirath@gmail.com>
2021-11-23 14:40:33 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -12,6 +13,11 @@ var enemyAttack = 12;
 var fight = function() {
   // Alert players that they are starting the round  
   window.alert("Welcome to Robot Gladiators!");
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+  // if player choses to fight, then fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+
 
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
@@ -28,21 +34,20 @@ var fight = function() {
   else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  playerHealth = playerHealth - enemyAttack;
-  
-  // Log a resulting message to the console so we know that it worked.
-  console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-  );
+  // if player choses to skip
+}
+  else if (promptFight === "skip" || promptFight === "SKIP") {
+      //confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you want to quit?");
 
-  //check players's health
-  if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
+      // if yes (true), leave fight
+      window.alert(playerName + " has decided to skip the fight. Goodbye!");
+      //subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
   }
 
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
+  else {
+      fight();
   }
 };
 
